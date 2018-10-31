fddb1e58dfe1d83f5b715dfde7dc3eacf2ca323e
Антипов Денис Александрович <eugenemcutalk@gmail.com>
2018-11-01 00:00:24 +0300

    Работай бл нах, третья строка добавлена типа

---
+++ emptyXyinyaConsole/emptyXyinyaConsole/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace emptyXyinyaConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Потому что я бэтмен");
+            Console.Write(", а Сазон - пидр)");
+            //третья строчка лалалала
+        }
+    }
+}
