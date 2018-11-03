3880b9d31b5979105433826fe227efa1908e9517
Евгеша <eugenemcutalk@gmail.com>
2018-11-03 03:20:03 +0300

    Иконка

--- SomeProject/SomeProject/Properties/AssemblyInfo.cs
+++ SomeProject/SomeProject/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
-[assembly: AssemblyTitle("SomeProject")]
+[assembly: AssemblyTitle("Антипов Д.А и Гнездилов М.К")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SomeProject")]
+[assembly: AssemblyCompany("Политех")]
+[assembly: AssemblyProduct("Курсовой проект, гр. 402")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
