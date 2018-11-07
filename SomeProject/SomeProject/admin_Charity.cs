2911b05bf45ec8534f77306288ee749b8cd82c40
Студент с кампутера <xyz12tyubv3@mail.ru>
2018-11-07 08:44:28 +0400

    Добавлена анальная вагинап пастрижися

--- SomeProject/SomeProject/admin_Charity.cs
+++ SomeProject/SomeProject/admin_Charity.cs
@@ -15,6 +15,7 @@ namespace SomeProject
     {
         public admin_Charity()
         {
+            // Хуй и анальная вагина
             InitializeComponent();
             CharityLoad();
             timer1.Tick += timer1_Tick;
