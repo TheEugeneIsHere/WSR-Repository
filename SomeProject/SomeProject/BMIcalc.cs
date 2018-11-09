2f4b11be79b8029a6d2b1d47273977651065cd63
Евгеша <eugenemcutalk@gmail.com>
2018-11-09 04:33:15 +0300

    литл-фиксы таймера

--- SomeProject/SomeProject/BMIcalc.cs
+++ SomeProject/SomeProject/BMIcalc.cs
@@ -29,7 +29,7 @@ namespace SomeProject
         private void timer1_Tick(object sender, EventArgs e)
         {
             TimeSpan timeremaining = connection.voteTime - DateTime.Now;
-            metroLabel1.Text = timeremaining.Days + " дней " + timeremaining.Hours +
+            metroLabel4.Text = timeremaining.Days + " дней " + timeremaining.Hours +
             " часов и " + timeremaining.Minutes + " минут до сдачи курсового";
         }
 
