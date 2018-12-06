c8db7847c2194437b73bd23c210fc6be671c8a47
Евгеша <eugenemcutalk@gmail.com>
2018-12-06 23:19:43 +0300

    Minor

--- WSR Project/WSR Project/Program.cs
+++ WSR Project/WSR Project/Program.cs
@@ -5,9 +5,6 @@ namespace WSRProject
 {
     static class Program
     {
-        /// <summary>
-        /// Главная точка входа для приложения.
-        /// </summary>
         [STAThread]
         static void Main()
         {
